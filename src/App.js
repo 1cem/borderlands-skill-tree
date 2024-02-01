55a6cc2184af05765096cffd1f23993e6d4720ac
Bircem Ari Kur <kurbircem@gmail.com>
2024-02-01 13:13:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import { SkillProvider, SkillTreeGroup, SkillTree } from 'beautiful-skill-tree';
 import logo from "./logo.png";
 import "./App.css";
 
+// Skill tree data
+const data = [{
+  id: 'first-skill',
+  title: 'The root node',
+  tooltip: {
+    description: "The parent node, all of the descendants will be locked until it's selected",
+  },
+  children: [
+    {
+      id: 'second-skill',
+      title: 'Second Skill',
+      tooltip: {
+        description: 'This is the second skill.',
+      },
+      children: [
+        {
+          id: 'third-skill',
+          title: 'Third Skill',
+          tooltip: {
+            description: 'This is the third skill.',
+          },
+          children: []
+        }
+      ]
+    }
+  ]
+}];
+
 function App() {
   return (
     <div className="App">
@@ -17,13 +45,12 @@ function App() {
       <SkillProvider>
         <SkillTreeGroup>
           {() => (
-            <SkillTree treeId="basic-birch" title="First Skill Tree" data={[]} />
+            <SkillTree treeId="basic-birch" title="First Skill Tree" data={data} />
           )}
         </SkillTreeGroup>
       </SkillProvider>
     </div>
   );
 }
-
 
 export default App;
